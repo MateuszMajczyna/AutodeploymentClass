84c9cfd7a675d89b854e34b5084ed167485ee773
Mateusz <majczynamateusz3@gmail.com>
2024-06-25 22:31:37 +0200

    Unit test code

--- Testing.py
+++ Testing.py
@@ -1,8 +1,19 @@
-#Test
 import unittest
 
-a = 11
-b = 7
-c = b + a
-print(c)
-print(a)
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        # Test data
+        a = 5
+        b = 10
+        expected_sum = 15
+
+        # Perform the test
+        result = a + b
+
+        # Assert that the result is equal to the expected sum
+        self.assertEqual(result, expected_sum, "The addition result should equal the expected sum")
+
+
+if __name__ == '__main__':
+    unittest.main()
